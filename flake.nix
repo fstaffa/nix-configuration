c0a9a1ebe25b10bd12ff81d46cf38e5be1f1af41
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2022-10-31 22:13:55 +0100

    working home manager on linux

--- flake.nix
+++ flake.nix
@@ -1,65 +1,60 @@
 {
-  description = "stskeygen for cimpress aws";
+  description = "Home manager configuration";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    personal-packages.url = "github:fstaffa/nix-packages";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, emacs-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        stskeygen = import ./packages/stskeygen.nix {inherit nixpkgs system; };
-        terminal-helpers = with import nixpkgs {system = system;};
-          [
-            tmux
-            thefuck
-            zoxide
-            starship
-            broot
-            fzf
-            tealdeer
-            bat
-            mcfly
-          ];
-        gui = with import nixpkgs {system = system;};
-          if stdenv.isLinux then
-            [
-              flameshot
-              xclip
-            ]
-          else [];
-        emacs = with import nixpkgs {system = system; overlays = [emacs-overlay.overlay];};
-          [
-            aspell
-            ripgrep
-            fd
-            curl
-            emacsGit
-          ];
-        work =  with import nixpkgs {system = system; config.allowUnfree = true;};
-        [
-          stskeygen
-          awscli2
-          ssm-session-manager-plugin
-        ];
-      in
-        {
-          formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-          packages.stskeygen = stskeygen;
+  outputs = { self, home-manager, nixpkgs, flake-utils, emacs-overlay, personal-packages }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    in
+    rec {
+      # Devshell for bootstrapping
+      # Accessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
+      });
 
-          packages.common = with import nixpkgs {system = system;}; buildEnv {
-            name = "home-env";
-            paths = [
-              tokei
-              fnm
-              ripgrep
-              jq
-              curl
-              aria
-              graphviz
-              shellcheck
-            ] ++ terminal-helpers ++ gui ++ work ++ emacs;
-          };
+      # This instantiates nixpkgs for each system listed above
+      # Allowing you to add overlays and configure it (e.g. allowUnfree)
+      # Our configurations will use these instances
+      # Your flake will also let you access your package set through nix build, shell, run, etc.
+      legacyPackages = forAllSystems (system:
+        import inputs.nixpkgs {
+          inherit system;
+          # This adds our overlays to pkgs
+          overlays = [ emacs-overlay.overlay ];
+
+          # NOTE: Using `nixpkgs.config` in your NixOS config won't work
+          # Instead, you should set nixpkgs configs here
+          # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
+          config.allowUnfree = true;
         }
-    );
+      );
+
+      homeConfigurations = {
+        "mathematician314@iguana" = home-manager.lib.homeManagerConfiguration {
+          pkgs = legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; personal-packages = personal-packages.packages.x86_64-linux; }; # Pass flake inputs to our config
+          # > Our main home-manager configuration file <
+          modules = [ ./home-manager/home.nix ];
+
+        };
+      };
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+    };
 }
