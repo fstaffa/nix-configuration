dc09a5d8197418f6da2689fc691d4a30f869c974
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2023-02-11 18:03:14 +0100

    macos settings, updated dependencies

--- darwin-configurations/hosts/macbook-work/default.nix
+++ darwin-configurations/hosts/macbook-work/default.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   services.nix-daemon.enable = true;
+
   security.pam.enableSudoTouchIdAuth = true;
 
   system.defaults.dock.autohide = true;
@@ -14,4 +16,14 @@
 
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToControl = true;
+
+  # smart keyboard
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+
+  #corresponding sliders KeyRepeat: 120, 90, 60, 30, 12, 6, 2
+  system.defaults.NSGlobalDomain.KeyRepeat = 2;
+  #corresponding sliders InitialKeyRepeat: 120, 94, 68, 35, 25, 15
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
 }
