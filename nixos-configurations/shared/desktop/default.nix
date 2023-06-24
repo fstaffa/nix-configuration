3c4f8dd4a7e80f1add52e383fa2442f46a87b3c8
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2023-06-24 22:58:53 +0200

    add firewall rules for syncthing

--- nixos-configurations/shared/desktop/default.nix
+++ nixos-configurations/shared/desktop/default.nix
@@ -56,4 +56,8 @@
       workstation = true;
     };
   };
+
+  #synthcing firewall
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
