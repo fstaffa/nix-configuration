b95a3de404c4017999eb6ec4e0c44c26cee2c812
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2024-06-25 12:49:34 +0200

    udpate packages, update gitlab token

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home manager configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
@@ -19,7 +19,7 @@
     };
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
