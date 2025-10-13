26832a2dbacc0c5d50531e55b3171eef714497c5
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2025-10-13 14:08:42 +0200

    remove ozon platform hint, remove squeezelite

--- home-manager/shared/main-desktop/default.nix
+++ home-manager/shared/main-desktop/default.nix
@@ -32,31 +32,8 @@
     quickemu
   ];
 
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
-
   services.syncthing.enable = true;
   services.opensnitch-ui.enable = true;
-
-  systemd.user.services.squeezelite = {
-    Unit = {
-      Description = "User-level Squeezelite Service";
-      After = [
-        "pipewire.service"
-        "network-online.target"
-      ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.squeezelite-pulse}/bin/squeezelite-pulse -n roon-target -s 192.168.10.208";
-      Restart = "on-failure";
-      RestartSec = "10";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 
   programs.firefox = {
     enable = true;
