d5bc19e84446d47a2ccaa0aec07824164a6b8040
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2026-08-22 17:11:42 +0200

    fix bruno-appimage build after appimageTools API change
    extractType2 was removed/renamed to extract in nixpkgs.

--- packages/bruno-appimage/default.nix
+++ packages/bruno-appimage/default.nix
@@ -16,7 +16,7 @@ let
     hash = platformData.hash;
   };
 
-  appimageContents = appimageTools.extractType2 { inherit pname version src; };
+  appimageContents = appimageTools.extract { inherit pname version src; };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
